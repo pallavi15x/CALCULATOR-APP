b55da597b7fde0094eec3153b7c249d6b5f612bf
Pallavi kumari <pallavikumari15102007@gmail.com>
2026-03-18 15:48:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+let display = document.getElementById("display");
+let history = document.getElementById("history");
+
+function append(value) {
+    display.value += value;
+
+    try {
+        history.innerText = "= " + eval(display.value);
+    } catch {
+        history.innerText = "";
+    }
+}
+
+function clearDisplay() {
+    display.value = "";
+    history.innerText = "";
+}
+
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+    try {
+        let result = eval(display.value);
+
+        if (result === Infinity) {
+            display.value = "Cannot divide by 0";
+            return;
+        }
+
+        history.innerText = display.value;
+        display.value = result;
+    } catch {
+        display.value = "Error";
+    }
+}
+
+/* Theme */
+function toggleTheme() {
+    document.body.classList.toggle("light");
+
+    let btn = document.getElementById("themeBtn");
+    btn.innerText = document.body.classList.contains("light") ? "☀️" : "🌙";
+}
+
+/* Keyboard support */
+document.addEventListener("keydown", (e) => {
+    if (!isNaN(e.key) || "+-*/.%".includes(e.key)) {
+        append(e.key);
+    } else if (e.key === "Enter") {
+        calculate();
+    } else if (e.key === "Backspace") {
+        deleteLast();
+    } else if (e.key === "Escape") {
+        clearDisplay();
+    }
+});
